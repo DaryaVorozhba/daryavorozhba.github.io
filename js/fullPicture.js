ed625456120a86deb3d03166f085ef79951971a6
DaryaVorozhba <darya.vorozhba@yandex.ru>
2024-03-07 11:57:47 +0300

    8.14. Открывается и закрывается 2

--- js/fullPicture.js
+++ js/fullPicture.js
@@ -1,13 +1,3 @@
-// Окно должно открываться при клике на миниатюру. Данные для окна (изображение, комментарии, лайки и так далее) берите из того же объекта, который использовался для отрисовки соответствующей миниатюры.
-// Для отображения окна нужно удалять класс hidden у элемента .big-picture и каждый раз заполнять его данными о конкретной фотографии:
-// Адрес изображения url подставьте как src изображения внутри блока .big-picture__img.
-// Количество лайков likes подставьте как текстовое содержание элемента .likes-count.
-// Количество показанных комментариев подставьте как текстовое содержание элемента .social__comment-shown-count.
-// Общее количество комментариев к фотографии comments подставьте как текстовое содержание элемента .social__comment-total-count.
-// Список комментариев под фотографией: комментарии должны вставляться в блок .social__comments.
-// Описание фотографии description вставьте строкой в блок .social__caption.
-// Подключите модуль в проект.
-
 import {isEscapeKey} from './util.js';
 import { simularPictures } from './pictures.js';
 
@@ -18,6 +8,11 @@ const body = document.querySelector('body');
 const commentCount = bigPicture.querySelector('.social__comment-count');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const socialComments = bigPicture.querySelector('.social__comments');
+const commentsShownCount = document.querySelector('.social__comment-shown-count');
+const commentsTotalCount = document.querySelector('.social__comment-total-count');
+
+let showCommentsCounter = 0;
+let pictureData;
 
 const onEskapeKey = (evt) => {
   if (isEscapeKey(evt)) {
@@ -30,58 +25,70 @@ const openBigPicture = function() {
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
   document.addEventListener('keyup', onEskapeKey);
-  commentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+  commentCount.classList.remove('hidden');
 };
 
 const onBigPictureBtnClose = function() {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keyup', onEskapeKey);
-  commentCount.classList.remove('hodden');
-  commentsLoader.classList.remove('hodden');
+  commentCount.classList.add('hidden');
 };
 
-const fillBigPicture = function(pictureData) {
-  bigPicture.querySelector('.big-picture__img').querySelector('img').src = pictureData.url;
-  bigPicture.querySelector('.likes-count').textContent = pictureData.likes;
-  bigPicture.querySelector('.social__comment-shown-count').textContent = pictureData.description;
-  bigPicture.querySelector('.social__comment-total-count').textContent = pictureData.comments.length;
+const fillBigPicture = function(data) {
+  bigPicture.querySelector('.big-picture__img').querySelector('img').src = data.url;
+  bigPicture.querySelector('.likes-count').textContent = data.likes;
+  bigPicture.querySelector('.social__comment-shown-count').textContent = data.description;
+  bigPicture.querySelector('.social__comment-total-count').textContent = data.comments.length;
 };
 
-const fillCommentsOnBigPicture = function(pictureData) {
+const showMoreComments = function(data) {
+  showCommentsCounter += 5;
+  const arrayComments = data.comments;
+  const showComments = [];
+
+  for (let i = 0; i < showCommentsCounter; i++) {
+    showComments.push(arrayComments[i]);
+
+    if (showCommentsCounter >= arrayComments.length) {
+      commentsLoader.classList.add('hidden');
+      showCommentsCounter = arrayComments.length;
+    } else {
+      commentsLoader.classList.remove('hidden');
+    }
+  }
+
   socialComments.innerHTML = '';
-  const comments = pictureData.comments;
-  socialComments.innerHTML = comments.map((comment) =>
+  socialComments.innerHTML = showComments.map((comment) =>
     `<li class="social__comment">
-      <img
-        class="social__picture"
-        src="${comment.avatar}"
-        alt="${comment.name}"
-        width="35" height="35">
-      <p class="social__text">${comment.message}</p>
-    </li>`
+         <img
+           class="social__picture"
+           src="${comment.avatar}"
+           alt="${comment.name}"
+           width="35" height="35">
+         <p class="social__text">${comment.message}</p>
+       </li>`
   ).join('');
+
+  commentsShownCount.textContent = showCommentsCounter;
+  commentsTotalCount.textContent = arrayComments.length;
 };
+
+const onCommentsLoaderClick = () => showMoreComments(pictureData);
 
 const onPicturesContainerClick = (event) => {
+  showCommentsCounter = 0;
   const targetId = event.target.parentNode.id;
-  const pictureData = simularPictures.find((element) => element.id === Number(targetId));
-  const pictureData = simularPictures.find((element) => {
-    window.console.log('elementId: ', element.id);
-    window.console.log('targetId: ', targetId);
-    return Number(element.id) === Number(targetId);
-  });
-  window.console.log(pictureData);
-  window.console.log(simularPictures);
+  pictureData = simularPictures.find((element) => element.id === Number(targetId));
 
   if (event.target.classList[0] === 'picture__img') {
     openBigPicture();
   }
 
   fillBigPicture(pictureData);
-  fillCommentsOnBigPicture(pictureData);
+  showMoreComments(pictureData);
 };
 
 picturesContainer.addEventListener('click', onPicturesContainerClick);
 bigPictureBtnClose.addEventListener('click', onBigPictureBtnClose);
+commentsLoader.addEventListener('click', onCommentsLoaderClick);
